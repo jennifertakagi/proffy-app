76b3c9323e824cf98771bad3331f346ed707446b
jennifertakagi <jennitakagi@gmail.com>
2020-08-08 14:30:21 -0300

    Add TeacherForm component and style

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader'
+
+import './styles.css'
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-list" className="container">
+           <PageHeader title="That's amazing that you want to teach!" />
+        </div>
+    );
+}
+
+export default TeacherForm;
+  
