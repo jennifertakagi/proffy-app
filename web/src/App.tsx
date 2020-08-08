8ceec32fa805b86528cbb30c3d0f0506e66f6c19
jennifertakagi <jennitakagi@gmail.com>
2020-08-08 14:29:01 -0300

    Add routes and global styles

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routes from '../../web/src/routes';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
