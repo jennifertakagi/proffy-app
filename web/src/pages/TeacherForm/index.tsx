e9051b47f8502c73a793b2ed04a3219188eb99da
jennifertakagi <jennitakagi@gmail.com>
2020-08-09 15:08:56 -0300

    Add data on TeacherForm and states

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
+
+import warningIcon from '../../assets/icons/warning.svg'
 
 import './styles.css'
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItem] = useState([
+        { week_day: 0, from: '', to: '' }
+    ]);
+
+    function addNewScheduleItem () {
+        setScheduleItem([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ]);
+    }
+
     return (
         <div id="page-teacher-form" className="container">
            <PageHeader 
@@ -16,21 +32,75 @@ function TeacherForm() {
                 <fieldset>
                     <legend>Your personal data</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Complete name</label>
-                        <input type="text" id="name" />
-                   </div>
-
-                   <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar" />
-                   </div>
-
-                   <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type="text" id="whatsapp" />
-                   </div>
+                    <Input name="name" label="Complete name" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biography"/>
                 </fieldset>
+
+                <fieldset>
+                    <legend>About the class</legend>
+                    
+                    <Select 
+                        name="subject" 
+                        label="Subject"
+                        options={[
+                            { value: 'Angular JS', label: 'Angular JS' },
+                            { value: 'Cypress', label: 'Cypress' },
+                            { value: 'Javascript', label: 'Javascript' },
+                            { value: 'Jest', label: 'Jest' },
+                            { value: 'Molecular JS', label: 'Molecular JS' },
+                            { value: 'Mongo DB', label: 'Mongo DB' },
+                            { value: 'Node JS', label: 'Node JS' },
+                            { value: 'React', label: 'React' },
+                            { value: 'React Native', label: 'React Native' },
+                            { value: 'Vue JS', label: 'Vue JS' }
+                        ]}
+                    />
+                    <Input name="cost" label="Cost of your class per hour" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Available schedules
+                        <button type="button" onClick={ addNewScheduleItem } >
+                            + New schedule
+                        </button>
+                    </legend>
+                    
+                        {scheduleItems.map(scheduleItem => {
+                            return (
+                                <div key={ scheduleItem.week_day } className="schedule-item">
+                                    <Select 
+                                        name="week_day" 
+                                        label="Week Day"
+                                        options={[
+                                            { value: '0', label: 'Sunday' },
+                                            { value: '1', label: 'Monday' },
+                                            { value: '2', label: 'Tuesday' },
+                                            { value: '3', label: 'Wednesday' },
+                                            { value: '4', label: 'Thursday' },
+                                            { value: '5', label: 'Friday' },
+                                            { value: '6', label: 'Saturday' }
+                                        ]}
+                                    />
+                                    <Input name="from" label="From" type="time" />
+                                    <Input name="to" label="To" type="time" />
+                                </div>
+                            )
+                        })}
+                       
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={ warningIcon } alt="Important warning" />
+                        Warning! <br />
+                        Fill in all the data
+                    </p>
+                    <button type="button">
+                        Save register
+                    </button>
+                </footer>
             </main>
         </div>
     );
