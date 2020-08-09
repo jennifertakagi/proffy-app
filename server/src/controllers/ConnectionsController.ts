e92e9814d6a6dcec330e5ffb78f39a91230ba46c
jennifertakagi <jennitakagi@gmail.com>
2020-08-09 13:17:34 -0300

    Add controllers and utils to convert hour to minutes

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, request } from 'express';
+import db from '../database/connection';
+
+export default class ConnectionsController {
+    async listConnection(request: Request, response: Response) {
+        const totalConnections = await db('connections').count('* as total');
+        const { total } = totalConnections[0];
+
+        return response.json({ total });
+    }
+
+    async createConnection (request: Request, response: Response) {
+        const { user_id } = request.body;
+
+        await db('connections').insert({ user_id });
+
+        return response.status(201).send();
+    }
+}
