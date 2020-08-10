4e35256de7208c743cb4422868020be900c92350
jennifertakagi <jennitakagi@gmail.com>
2020-08-09 21:35:52 -0300

    Add Favorites component and style

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function Favorites() {
-    
+	const [favorites, setFavorites] = useState([]);
+
+	function loadFavorites() {
+		AsyncStorage.getItem('favorites')
+		.then(response => {
+			if (response) {
+				const favoritesTeachers = (JSON.parse(response));
+				setFavorites(favoritesTeachers);
+			}
+		});
+	}
+	
+	useFocusEffect(() => {
+		loadFavorites();
+	})
+
 	return (
 		<View style={ styles.container }>
 			<PageHeader title="My Favorite Proffys" />
+
+			<ScrollView 
+				style={ styles.teacherList }
+				contentContainerStyle={{
+					paddingHorizontal: 16,
+					paddingBottom: 16
+				}}
+			>
+				{favorites.map((teacher: Teacher) => {
+					return (
+						<TeacherItem
+							key={ teacher.id }
+							teacher={ teacher }
+							setAsFavorite
+						/>
+					)
+				})}
+			</ScrollView>
+			
 		</View>
 	);
 }
